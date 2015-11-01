79db79645e49fdfec4c3d5b25b41193d06bdbb8f
zhangshqui <18657672296@163.com>
2015-11-01 15:52:25 +0800

    创建了person类

---
+++ Person.java
@@ -0,0 +1,18 @@
+class Person{
+
+	String name;
+ 	int age;
+
+	public void eat(){
+
+
+		System. out.println("�ԳԳ�");
+
+}
+
+    public  void sleep(){
+
+     System. out . println("˭˭˭");
+}
+
+}
